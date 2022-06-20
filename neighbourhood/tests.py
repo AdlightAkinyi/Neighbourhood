087841a67c1931d4fc3ca4a1c91715a4bd6f28fb
C0159412020 <adlightakinyi1@gmail.com>
2022-06-20 16:35:59 +0300

    created test

--- neighbourhood/tests.py
+++ neighbourhood/tests.py
@@ -1,70 +1,99 @@
 from django.test import TestCase
 from .models import *
-from django.contrib.auth.models import User
 
 # Create your tests here.
-class ProfileTest(TestCase):
+
+class NeighbourhoodTestClass(TestCase):
+    # Set up method
     def setUp(self):
-        self.user = User(username = 'kevin_sniper', email = 'kevin@kevin.com', password = 'passwadd')
-        self.user.save()
-        self.kevin = Profile(bio = 'A python Programmer',contact = '054234444', user = self.user)
+        self.Juja = Location.objects.create(name="Juja")
+        self.test_neighbourhood = Neighbourhood.objects.create(name='Membley',location='Nairobi',admin='Chacha',image='example.jpg',description='Beautiful place',occupants='1')
+        self.test_neighbourhood.save()
 
+
+    def test_save_method(self):
+        self.test_neighbourhood.save()
+        test_neighbourhoods = Neighbourhood.objects.all()
+        self.assertTrue(len(test_neighbourhoods) > 0)
+            
+    
+    def test_delete_method(self):
+        self.Neighbourhood.delete_neighbourhood()
+        neighbourhoods = Neighbourhood.objects.all()
+        self.assertTrue(len(neighbourhoods)==0)   
+        
     def tearDown(self):
-        Profile.objects.all().delete()
-
-    def test_instance(self):
-        self.assertTrue(isinstance(self.kevin,Profile))
-
-    def test_save(self):
-        self.kevin.create_user_profile(self.user,True)
-        self.kevin.save_user_profile(self.user)
-        users = Profile.objects.all()
-        self.assertTrue(len(users)>0)
-
-
-class HoodTest(TestCase):
+        Neighbourhood.objects.all().delete()
+        
+        
+class ProfileTestClass(TestCase):
+    # Set up method
     def setUp(self):
-        self.user = User(username='kevin_sniper', email='kevin@kevin.com', password='passwadd')
-        self.user.save()
-        self.kevin = Profile(bio='A python Programmer', contact='054234444', user=self.user)
-        self.hood = Hood(name = 'Ngong',bio = "Milimani",admin = self.user)
-
-    def tearDown(self):
-        Profile.objects.all().delete()
-        self.hood.delete()
-
+        self.Prof= Profile( id = '1', user ='James',bio='Happy', profile_picture = 'example.jpg',email='chacha@gmail.com',phone_number='0724580020',neighbourhood='Membley' )
+        
+    # Testing  instance
     def test_instance(self):
-        self.assertTrue(isinstance(self.hood,Hood))
-
-    def test_save(self):
-        self.hood.save_hood()
-        hoods = Hood.objects.all()
-        self.assertTrue(len(hoods) == 1)
-
-
-
-class PostTest(TestCase):
+        self.assertTrue(isinstance(self.Prof,Profile)) 
+        
+        
+class PostTestClass(TestCase):
+    # Set up method
     def setUp(self):
-        self.user = User(username='kevin_sniper', email='kevin@kevin.com', password='passwadd')
-        self.user.save()
-        self.kevin = Profile(bio='A python Programmer', contact='054234444', user=self.user)
-        self.hood = Hood(name='Ngong', bio="Milimani", admin=self.user)
-        self.business = Business(name="brian", owner = self.user, business_description= 'langat',
-                                 locale = self.hood,business_number = 4322323)
-        self.post = Post(title='Postings',post = 'This is the post',
-                         hood = self.hood, poster = self.user)
-
-    def tearDown(self):
-        Profile.objects.all().delete()
-        self.hood.delete()
-        self.business.delete()
-        self.post.delete()
-
+        self.Pos= Post( id = '1', user ='James',title='Happy', info = 'Happy day',neighbourhood='Membley' )
+        
+    # Testing  instance
     def test_instance(self):
-        self.assertTrue(isinstance(self.post,Post))
+        self.assertTrue(isinstance(self.Pos,Post))        
+                         
+class CommentTestClass(TestCase):
+    # Set up method
+    def setUp(self):
+        self.Comm= Post( id = '1', comment ='Nice',post='Happy', user = 'Chacha')
+        
+    # Testing  instance
+    def test_instance(self):
+        self.assertTrue(isinstance(self.Comm,Comment)) 
+        
 
-    def test_save(self):
-        self.post.save_post()
-        posts = Post.objects.all()
-        self.assertTrue(len(posts) == 1)
+class AuthorityTestClass(TestCase):
+    # Set up method
+    def setUp(self):
+        self.Auth= Post( id = '1', name ='Police',email='police@gmail.com', contact = '707594873',neighbourhood='Membley' )
+        
+    # Testing  instance
+    def test_instance(self):
+        self.assertTrue(isinstance(self.Auth,Authority))
+        
 
+class HealthTestClass(TestCase):
+    # Set up method
+    def setUp(self):
+        self.Heal= Heal( id = '1', name ='Hospital',email='hospital@gmail.com', contact = '707594873',neighbourhood='Membley' )
+        
+    # Testing  instance
+    def test_instance(self):
+        self.assertTrue(isinstance(self.Heal,Health))   
+        
+        
+class BusinessTestClass(TestCase):
+    # Set up method
+    def setUp(self):
+        self.Busi = Location.objects.create(name="Busi")
+
+        self.test_business = Business.objects.create(name='Barber',image='example.jpg',user='Chacha',email='example@gmail.com',phone_number='072565676',neighbourhood='Membley')
+
+        self.test_business.save()
+
+    def test_save_method(self):
+        self.test_business.save()
+        test_business = Business.objects.all()
+        self.assertTrue(len(test_business) > 0)
+            
+    
+    def test_delete_method(self):
+        self.Business.delete_business()
+        business = Business.objects.all()
+        self.assertTrue(len(business)==0)   
+        
+    def tearDown(self):
+        Business.objects.all().delete()                                             
